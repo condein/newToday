e261e8b8c4cd609bc280cc2c659e13ccea1ca637
condein <raushanverma95086@gmail.com>
2024-01-28 14:14:25 +0530

    update

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 const progressVal = document.querySelector(".progress-value");
 const error = document.querySelector(".error");
 const circle = document.querySelectorAll(".circle");
-const inputs = document.querySelectorAll("input");
+const first_circle = document.querySelector(".first-circle");
+const second_circle = document.querySelector(".second-circle");
+const third_circle = document.querySelector(".third-circle");
+const first_input = document.querySelector(".first-input");
+const second_input = document.querySelector(".second-input");
+const third_input = document.querySelector(".third-input");
 
-inputs.forEach((e) => {
-  e.addEventListener("keyup", (e) => {
-    if (e.target.value.length !== 0) {
-      circle.forEach((e) => {
-        e.addEventListener("click", () => {
-          e.classList.toggle("clicked");
-          progressVal.style.width = "90%";
-        });
-      });
-    }
-  });
+/////first function ///
+
+first_input.addEventListener("keyup", (e) => {
+  if (e.target.value.length !== 0) {
+    first_circle.addEventListener("click", () => {
+      first_circle.classList.toggle("clicked");
+      progressVal.style.width = "35%";
+    });
+  }
 });
 
-inputs.forEach((e) => {
-  e.addEventListener("keyup", (e) => {
-    if (e.target.value.length === 0) {
-      circle.forEach((e) => {
-        e.addEventListener("click", () => {
-          console.log("clicked");
-          error.style.display = "block";
-        });
-      });
-    }
-  });
+second_input.addEventListener("keyup", (e) => {
+  if (e.target.value.length !== 0) {
+    second_circle.addEventListener("click", () => {
+      second_circle.classList.toggle("clicked");
+      progressVal.style.width = "70%";
+    });
+  }
+});
+
+third_input.addEventListener("keyup", (e) => {
+  if (e.target.value.length !== 0) {
+    third_circle.addEventListener("click", () => {
+      third_circle.classList.toggle("clicked");
+      progressVal.style.width = "100%";
+    });
+  }
 });
